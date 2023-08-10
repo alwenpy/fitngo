d35176a7f6f9b2de459ca5d26397516328f14c0c
alwenpy <alwen1928@gmail.com>
2023-08-10 19:43:56 +0530

    cache files deleted

--- home/views.py
+++ home/views.py
@@ -2,12 +2,10 @@ from django.shortcuts import render
 from blog.models import *
 from django.views.decorators.cache import cache_page
 
-@cache_page(60 * 1)
 def index(request):
     posts=Blog.objects.all()
     context={"posts":posts}
     return render(request,'index-4.html',context)
 
-@cache_page(60 * 1)
 def appointment(request):
     return render(request,'appointment.html')
