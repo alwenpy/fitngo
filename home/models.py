d193fa47ee0d1a0d231bada52f21fb9ec3941632
alwenpy <alwen1928@gmail.com>
2023-08-16 23:39:59 +0530

    testimonial section dynamic

--- home/models.py
+++ home/models.py
@@ -1,3 +1,13 @@
 from django.db import models
+from django.core.validators import MaxValueValidator
 
-# Create your models here.
+class Testimonial(models.Model):
+    name = models.CharField(max_length=100)
+    occupation = models.CharField(max_length=100)
+    content = models.TextField()
+    image = models.ImageField(upload_to='testimonial/images', null=True, blank=True,)
+    star_rating = models.PositiveIntegerField(default=0, verbose_name='Star Rating', validators=[MaxValueValidator(5)])
+    created_at = models.DateTimeField(auto_now_add=True)
+
+    def __str__(self):
+        return self.name
--- home/views.py
+++ home/views.py
@@ -1,10 +1,12 @@
 from django.shortcuts import render
 from blog.models import *
+from home.models import *
 from django.views.decorators.cache import cache_page
 
 def index(request):
     posts=Blog.objects.all()
-    context={"posts":posts}
+    testimonials=Testimonial.objects.all()
+    context={"posts":posts,"testimonials":testimonials}
     return render(request,'index-4.html',context)
 
 def appointment(request):
