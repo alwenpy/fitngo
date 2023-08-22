f2c2d3a57bb45767831b94f6185dcf6ac0cdf745
alwenpy <alwen1928@gmail.com>
2023-08-22 14:44:41 +0530

    typos

--- home/models.py
+++ home/models.py
@@ -6,8 +6,9 @@ class Testimonial(models.Model):
     occupation = models.CharField(max_length=100)
     content = models.TextField()
     image = models.ImageField(upload_to='testimonial/images', null=True, blank=True,)
-    star_rating = models.PositiveIntegerField(default=0, verbose_name='Star Rating', validators=[MaxValueValidator(5)])
+    star_rating = models.PositiveIntegerField(validators=[MaxValueValidator(5)])
     created_at = models.DateTimeField(auto_now_add=True)
+    
 
     def __str__(self):
         return self.name
